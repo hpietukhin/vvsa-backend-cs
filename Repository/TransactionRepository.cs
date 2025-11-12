cea59ab1bf65ca7c63f8cfcc9a92515e1c3cd2f4
Havriil Pietukhin <78024475+hpietukhin@users.noreply.github.com>
2025-11-12 17:11:50 +0100

    fixes (#2)
    * install nuget packages
    
    * namespace fix
    
    * rename
    
    ---------
    
    Co-authored-by: Havriil Pietukhin <havriil.pietukhin@firma.seznam.cz>

--- Repository/TransactionRepository.cs
+++ Repository/TransactionRepository.cs
@@ -1,39 +1,40 @@
-using MyWebApiWithControllers.Model;
+using MyWebApiWithControllers.DatabaseModel;
 
 namespace MyWebApiWithControllers.Repository;
 
-public class TransactionRepository : ITransactionRepository {
-    private List<Transaction> transactions;
 
-    public TransactionRepository() 
+
+public class TransactionRepository : ITransactionRepository
+{
+    private readonly Whiyes5oContext context;
+
+
+    public TransactionRepository(Whiyes5oContext context)
     {
-        transactions = new List<Transaction>{
-            new Transaction { TransactionId=1 },
-            new Transaction { TransactionId=2 }
-        };
+        this.context = context;
     }
 
-    public List<Transaction> GetAllTransactions() 
+    public List<Transaction> GetAllTransactions()
     {
-        return transactions;
+        return context.Transactions.ToList();
     }
 
-    public Transaction GetTransactionById(int id) 
+    public Transaction GetTransactionById(int id)
     {
-        return transactions.FirstOrDefault(t => t.TransactionId == id);
+        return context.Transactions.FirstOrDefault(t => t.Id == id);
     }
 
     public void Add(Transaction transaction)
     {
-        transactions.Add(transaction);
+        context.Transactions.Add(transaction);
     }
 
-    public void DeleteById(int id) 
+    public void DeleteById(int id)
     {
-        var transaction = transactions.FirstOrDefault(t => t.TransactionId == id);
+        var transaction = context.Transactions.FirstOrDefault(t => t.Id == id);
         if (transaction != null)
         {
-            transactions.Remove(transaction);
+            context.Transactions.Remove(transaction);
         }
     }
 }
